60a6d9661a8e135bb5b237662cb24ae34fe567a5
xiaopengh <mathys.zhang@yahoo.com>
2022-03-13 23:48:07 +0100

    scr

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
 
 import pygame
-
+pygame.init()
 
 WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Jeu inédit")
+
+
+PINK = (255, 225, 255)
+
+FPS = 60
+
+
+def draw_window():
+    WIN.fill(PINK)
+    pygame.display.update()
 
 
 def main():
-
+    clock = pygame.time.Clock()
     running = True
     while running:
-
+        clock.tick(FPS)
         for event in pygame.event.get():
-            if event.type == pygame.quit():
+            if event.type == pygame.QUIT:
                 running = False
 
+        draw_window()
+
     pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
